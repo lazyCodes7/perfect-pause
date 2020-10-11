7714f934c7d8c93cbf177b897bd74e22bb97b0f4
Rishab Mudliar <rishabmudliar@gmail.com>
2020-10-11 10:29:43 +0530

    Makes pause work for eternity once paused

--- VLC/vlcscrip.py
+++ VLC/vlcscrip.py
@@ -20,14 +20,17 @@ media_player.set_media(media)
 media_player.audio_set_volume(100)
 # start playing video
 media_player.play()
-value = media_player.get_length()
-print(value)
+
 # wait so the video can be played for 5 seconds
 # irrespective for length of video
 time.sleep(5)
 
-# pausing the video
-media_player.set_pause(1)
-time.sleep(5)
-media_player.play()
-time.sleep(60)
+value = 1
+# printing value
+while(value):
+	media_player.set_pause(1)
+	time.sleep(1)
+	if(value==0):
+		media_player.play()
+	value=1
+# pausing the video 
