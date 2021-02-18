bb45d1193de36cd9ec3f4d3d53195638c77d60e4
siddharthc30 <csidhu141@gmail.com>
2021-02-18 22:45:51 +0530

    add desktop app layout

---
+++ app.py
@@ -0,0 +1,48 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'Perfect-Pause'
+        self.left = 500
+        self.top = 100
+        self.width = 500
+        self.height = 200
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+    
+        # Create textbox
+        self.textbox = QLineEdit(self)
+        self.textbox.move(20, 20)
+        self.textbox.resize(280,40)
+        
+        # Create a button in the window
+        self.button = QPushButton('Play', self)
+        self.button.move(20,80)
+        
+        # connect button to function on_click
+        self.button.clicked.connect(self.on_click)
+        self.show()
+    
+    @pyqtSlot()
+    def on_click(self):
+        textboxValue = self.textbox.text()
+        #QMessageBox.question(self, 'Message - pythonspot.com', "You typed: " + textboxValue, QMessageBox.Ok, QMessageBox.Ok)
+        self.textbox.setText("")
+
+        print(textboxValue)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
+
+
+
